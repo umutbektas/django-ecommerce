47f63dc75aaa8791f6ea0065d6cb36a34f6c9d60
Umut <umut.bektas07@gmail.com>
2020-02-04 19:18:48 +0300

    Forms & Admin for our Custom User

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,38 @@
 from django.contrib import admin
 from .models import GuestEmail, User
+from .forms import UserAdminCreationForm, UserAdminChangeForm
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    # The forms to add and change user instances
+    form = UserAdminChangeForm
+    add_form = UserAdminCreationForm
+
+    # The fields to be used in displaying the User model.
+    # These override the definitions on the base UserAdmin
+    # that reference specific fields on auth.User.
+    list_display = ('email', 'superuser', 'staff', 'active')
+    list_filter = ('superuser', 'staff', 'active')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': ('superuser', 'staff', 'active')}),
+    )
+    # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
+    # overrides get_fieldsets to use this attribute when creating a user.
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')}
+        ),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+    filter_horizontal = ()
+
 
 
 admin.site.register(GuestEmail)
 
-admin.site.register(User)
--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,54 @@
 from django import forms
 from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
 
 User = get_user_model()
+
+class UserAdminCreationForm(forms.ModelForm):
+    """
+    A form for creating new users. Includes all the required
+    fields, plus a repeated password.
+    """
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('email',)
+
+    def clean_password2(self):
+        # Check that the two password entries match
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super(UserAdminCreationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+
+class UserAdminChangeForm(forms.ModelForm):
+    """A form for updating users. Includes all the fields on
+    the user, but replaces the password field with admin's
+    password hash display field.
+    """
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = User
+        fields = ('email', 'password', 'active', 'superuser')
+
+    def clean_password(self):
+        # Regardless of what the user provides, return the initial value.
+        # This is done here, rather than on the field, because the
+        # field does not have access to the initial value
+        return self.initial["password"]
 
 
 class LoginForm(forms.Form):
@@ -9,7 +56,7 @@ class LoginForm(forms.Form):
         widget=forms.TextInput(
             attrs={
                 "class": "form-control",
-                "placeholder": "Username"
+                "placeholder": "E-Mail"
             }
         )
     )
@@ -28,7 +75,7 @@ class RegisterForm(forms.Form):
         widget=forms.TextInput(
             attrs={
                 "class": "form-control",
-                "placeholder": "Username"
+                "placeholder": "E-Mail"
             }
         )
     )
@@ -57,13 +104,6 @@ class RegisterForm(forms.Form):
             }
         )
     )
-
-    def clean_username(self):
-        username = self.cleaned_data.get('username')
-        qs = User.objects.filter(username=username)
-        if qs.exists():
-            raise forms.ValidationError('Username is taken')
-        return username
 
     def clean_email(self):
         email = self.cleaned_data.get('email')
